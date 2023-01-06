20302b123cc3bbe240f0c5d50b9dc6278590b9c5
Taylor <bKuzzcs@gmail.com>
2023-01-06 13:44:42 -0700

    commit tree d05fecc438b0b5cbfa546b32d1c1effca04d4ef2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require_relative '../config/environment'
 require_relative'../lib/student'
 require_relative'../lib/interactive_record'
 
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
